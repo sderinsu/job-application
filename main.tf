cb9c312a988d2bb63598af5338a07886e774729c
sderinsu <sderinsu@users.noreply.github.com>
2026-02-02 22:41:45 -0600

    devops: initial terraform application outputs

---
+++ main.tf
@@ -0,0 +1,9 @@
+output "applicant_name" {
+  description = "Name of the applicant"
+  value       = "Savas Derinsu"
+}
+
+output "position_title" {
+  description = "Position being applied for"
+  value       = "Senior DevOps Engineer"
+}
